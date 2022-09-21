325c0988fd44c991ad7a5075aa01e57bb03888c2
bakekaga <55259825+bakekaga@users.noreply.github.com>
2022-09-21 11:03:25 -0500

    Create Main.java

---
+++ 2048/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) throws IOException
+  {
+    File f = new File("bestScore.dat");
+    Game g = new Game(f);
+    
+    // add a call to Game method to begin game!
+
+    
+
+  }
+}
